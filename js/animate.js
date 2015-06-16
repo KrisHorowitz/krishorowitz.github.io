398280144b8ed88e7236c5c1d35eb390fd3869f5
Kris Horowitz <kris@readtapestry.com>
2015-06-16 16:17:47 -0400

    - Change fonts - Add job title, time at job - Fix bottom social icons

--- js/animate.js
+++ js/animate.js
@@ -1,14 +1,20 @@
-$('span').hover( function () {
-    $(".top").addClass('show');
-    setTimeout(function(){
-        $(".middle").addClass('show');
-    }, 300);
-    setTimeout(function(){
-        $(".bottom").addClass('show');
-    }, 600);
-  }, function() {
-    $(".top").removeClass('show');
-    $(".middle").removeClass('show');
-    $(".bottom").removeClass('show');
-  }
-);
+// function hover(popup, node){
+// console.log(popup, node);
+
+// $(popup).hover(
+//     function() {
+//         $(node).css({
+//             "background": "url('images/timeline-node-hover.png') center top no-repeat",
+//             "background-size": "20px 20px"
+//         });
+//     },
+//     function() {
+//         $(node).css({
+//             "background": "url('images/timeline-node.png') center top no-repeat",
+//             "background-size": "20px 20px"
+//         });
+//     }
+// );
+// }
+
+// hover("#betaworks", "#jan15");
