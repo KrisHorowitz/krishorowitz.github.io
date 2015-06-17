72971e83c087201ca883cf6a7cf61eb247d3e66d
Kris Horowitz <kris@readtapestry.com>
2015-06-17 11:33:48 -0400

    - Change copy - Add LinkedIn - Fix node hover states

--- js/animate.js
+++ js/animate.js
@@ -1,20 +1,38 @@
-// function hover(popup, node){
-// console.log(popup, node);
+function hover(popup, node){
+console.log(popup, node);
 
-// $(popup).hover(
-//     function() {
-//         $(node).css({
-//             "background": "url('images/timeline-node-hover.png') center top no-repeat",
-//             "background-size": "20px 20px"
-//         });
-//     },
-//     function() {
-//         $(node).css({
-//             "background": "url('images/timeline-node.png') center top no-repeat",
-//             "background-size": "20px 20px"
-//         });
+$(popup).hover(
+    function() {
+        $(node).css({
+            "background": "url('images/timeline-node-hover.png') center top no-repeat",
+            "background-size": "20px 20px"
+        });
+    },
+    function() {
+        $(node).css({
+            "background": "url('images/timeline-node.png') center top no-repeat",
+            "background-size": "20px 20px"
+        });
+    }
+);
+}
+
+hover("#betaworks", "#jan15");
+hover("#tap1", "#oct13");
+
+
+//look into
+// $(function() {
+//   $('a[href*=#]:not([href=#])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000);
+//         return false;
+//       }
 //     }
-// );
-// }
-
-// hover("#betaworks", "#jan15");
+//   });
+// });
